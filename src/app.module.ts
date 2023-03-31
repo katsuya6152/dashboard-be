b767bd103180643eb510e5747fe5bde4092bd885
katsuya6152 <katsuya.6152@gmail.com>
2023-03-31 17:26:50 +0900

    add cors middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -6,10 +6,21 @@ import { RawModule } from './raw/raw.module';
 import { PrismaMongoModule } from './prisma-mongo/prisma-mongo.module';
 import { ResultsModule } from './results/results.module';
 import { ProcessedDataModule } from './processed-data/processed-data.module';
+import { CorsMiddleware } from './common/middleware/cors/cors.middleware';
 
 @Module({
-  imports: [PrismaModule, RawModule, PrismaMongoModule, ResultsModule, ProcessedDataModule],
+  imports: [
+    PrismaModule,
+    RawModule,
+    PrismaMongoModule,
+    ResultsModule,
+    ProcessedDataModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorsMiddleware).forRoutes('*');
+  }
+}
